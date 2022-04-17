fa479acff947659bd480023becd7b4cc7bda098a
li_pb <13603834214@163.com>
2022-04-17 23:24:15 +0800

    feat first

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var App = function App() {
+    return React.createElement(
+        'div',
+        null,
+        'da'
+    );
+};
+var root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(App, null));
